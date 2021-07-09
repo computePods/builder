9639474c81038b3b60327073479e43da2658bde9
Stephen Gaito <stephen@perceptisys.co.uk>
2021-07-09 10:47:08 +0100

    removed the use of networkx package as it is too large and complex to use reliably for our simple problem

--- cpb/cekitModules/cpPyNatsFastAPI-apk/installCPPyNatsFastAPI.sh
+++ cpb/cekitModules/cpPyNatsFastAPI-apk/installCPPyNatsFastAPI.sh
@@ -7,7 +7,6 @@ echo $0
 echo "----------------------------------------------------------------------"
 echo ""
 
-pip install networkx
 pip install pyyaml
 pip install aiofiles
 pip install asyncio-nats-client
